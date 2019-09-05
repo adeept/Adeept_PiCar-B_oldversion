1e16eb7728c29a04c9381ee5d41c8f0c8cdf71dd
Adeept <support@adeept.com>
2019-09-05 10:47:47 +0800

    Update setup.py

--- server/setup.py
+++ server/setup.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-# File name   : motor.py
-# Description : Control Motors 
+# File name   : setup.py
 # Website     : www.adeept.com
 # E-mail      : support@adeept.com
 # Author      : William
@@ -154,20 +153,31 @@ except:
 for x in range(1,4):
 	if os.system("sudo apt-get install -y util-linux procps hostapd iproute2 iw haveged dnsmasq") == 0:
 		break
-
+'''
 try:
 	os.system('sudo mknod("//home/pi/.config/autostart/car.desktop")')
 	with open("//home/pi/.config/autostart/car.desktop",'w') as file_to_write:
 		file_to_write.write("[Desktop Entry]\n   Name=Car\n   Comment=Car\n   Exec=sudo python3 //home/pi/adeept_picar-b/server/server.py\n   Icon=false\n   Terminal=false\n   MutipleArgs=false\n   Type=Application\n   Catagories=Application;Development;\n   StartupNotify=true")
 except:
 	pass
-
+'''
 os.system("sudo cp -f //home/pi/adeept_picar-b/server/set.txt /home/pi/set.txt")
 
 try:
 	os.system("sudo pip3 install pocketsphinx")
 except:
 	pass
+
+try:
+	os.system('sudo touch //home/pi/startup.sh')
+	with open("//home/pi/startup.sh",'w') as file_to_write:
+		file_to_write.write("#!/bin/sh\nsleep 10s\nsudo python3 //home/pi/gtank/server/server.py")
+except:
+	pass
+
+os.system('sudo chmod 777 //home/pi/startup.sh')
+
+replace_num('/etc/rc.local','fi','fi\n//home/pi/startup.sh start')
 
 print('restarting')
 
