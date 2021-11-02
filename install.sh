af7c305fe6eeb566f2918d05ef8df6ca2970b43c
Ivan Marchand <ivan.marchand@gotruemotion.com>
2021-11-02 12:35:00 -0400

    Add install script

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ BASEDIR=$(dirname "$0")
 BASEDIR=$(cd $BASEDIR; pwd)
 
 cd $BASEDIR/webserver
-pipenv run python manage.py collectstatic --noinput
+pipenv install
 
 cd $BASEDIR/react/robot
-npm run build
+npm install
