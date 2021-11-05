365a0c0ba1d343eec8c3ad298afd985289f16d80
Ivan Marchand <ivan.marchand@gotruemotion.com>
2021-11-05 19:15:13 -0400

    fix

--- webserver/robot/arm.py
+++ webserver/robot/arm.py
@@ -13,9 +13,9 @@ class Arm:
     def lift_elbow(angle):
         config = Config.get_config()
         up = int(config.get('elbow_up', 400))
-        down = int(config.get('elbow_down', 200))
-        course = up - down
-        value = int(down + angle * (course / 90.0))
+        down = int(config.get('elbow_down', 600))
+        course = down - up
+        value = int(up + angle * (course / 90.0))
         Arm.PWM.set_pwm(3, 0, value)
 
     @staticmethod
